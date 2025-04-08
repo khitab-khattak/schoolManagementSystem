5c59dbd6cf8d208b3ada6d61a7d206dd063a1d39
Khitab-khattakgit commit -m first <khitabk29@gmail.com>
2025-04-08 08:08:31 +0500

    cleanup code

--- app/Http/Controllers/backend/DashboardController.php
+++ app/Http/Controllers/backend/DashboardController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     public function dashboard(){
-        return view('backend.dashboard');
+        $meta_title = 'Dashboard';
+        return view('backend.dashboard',compact('meta_title'));
     }
 }
--- resources/views/backend/layouts/app.blade.php
+++ resources/views/backend/layouts/app.blade.php
@@ -2,13 +2,15 @@
 <html lang="en">
     <head>
         <!-- META SECTION -->
-        <title>Atlant - Responsive Bootstrap Admin Template</title>            
+        <title>{{ !empty($meta_title) ? $meta_title : ''}} - School</title>            
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         
+        
         <!-- Favicon (icon) -->
-        <link rel="icon" href="{{ asset('assets/images/favicon.ico') }}" type="image/x-icon" />
+        <link rel="icon" href="{{ asset('assets/images/icon/favicon.ico') }}" type="image/x-icon" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
         <!-- END META SECTION -->
         
         <!-- CSS INCLUDE -->
